af5d80d40fd4534226102c47e9ad692270daa1e5
dante.fuentes2019@twk.cl <danteleganster15>
2020-06-11 02:37:58 -0400

    Vistas (acerca de la app,bienvenida) centradas e incorporadas sus funcionalidades en botones.

--- app/src/main/java/com/example/principal/acerca_de_la_app.java
+++ app/src/main/java/com/example/principal/acerca_de_la_app.java
@@ -2,7 +2,9 @@ package com.example.principal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class acerca_de_la_app extends AppCompatActivity {
 
@@ -11,4 +13,23 @@ public class acerca_de_la_app extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acerca_de_la_app);
     }
+
+    public void siguiente (View view)
+    {
+        Intent i = new Intent(this,Cuestionario.class);
+        startActivity(i);
+    }
+    public void Atras (View view)
+    {
+        Intent i = new Intent(this,bienvenida.class);
+        startActivity(i);
+    }
+
+
+
+
+
+
+
+
 }
